61879e915178f62a2430c7675d5594a8baa41dd0
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2025-03-08 22:02:28 +0200

    init project

---
+++ main.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const canvas = document.getElementById('myCanvas');
+canvas.width = 200;
+
+const ctx = canvas.getContext('2d');
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
+animate();
+
+function animate() {
+  canvas.height = window.innerHeight;
+  car.update();
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
