ac7ac92640709e0b580c1bffad0d9724881f2f39
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2025-03-11 08:50:57 +0200

    Car: multiple cars

--- car.js
+++ car.js
@@ -123,7 +123,7 @@ class Car {
     this.y -= this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = 'gray';
     } else {
@@ -137,7 +137,7 @@ class Car {
     }
     ctx.fill();
 
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
     }
   }
