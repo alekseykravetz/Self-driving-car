9cfbf8aa57510edf0644e454a24d87a717f907e1
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2025-03-11 10:11:46 +0200

    Network: Mutate from best car, fitness function, save and discard best car to local storage

--- main.js
+++ main.js
@@ -8,8 +8,27 @@ const carCtx = carCanvas.getContext('2d');
 const networkCtx = networkCanvas.getContext('2d');
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const cars = generateCars(100);
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
+const N = 1;
+const cars = generateCars(N);
+let bestCar = cars[0];
+if (localStorage.getItem('bestBrain')) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem('bestBrain'));
+    if (i !== 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.3);
+    }
+  }
+}
+
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(0), -500, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(1), -700, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(2), -700, 30, 50, 'DUMMY', 2),
+];
 
 animate();
 
@@ -21,6 +40,13 @@ function generateCars(n) {
   return cars;
 }
 
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
+}
+function remove() {
+  localStorage.removeItem('bestBrain');
+}
+
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
@@ -30,7 +56,8 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find((c) => c.y === Math.min(...cars.map((c) => c.y)));
+  // Fitness function
+  bestCar = cars.find((c) => c.y === Math.min(...cars.map((c) => c.y)));
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
