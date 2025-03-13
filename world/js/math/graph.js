db23945f1b08610892ca3a551ed2438ef62fba19
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2025-03-13 09:48:46 +0200

    basic graph, segments and points functionality

--- world/js/math/graph.js
+++ world/js/math/graph.js
@@ -4,6 +4,66 @@ class Graph {
     this.segments = segments;
   }
 
+  addPoint(point) {
+    this.points.push(point);
+  }
+
+  containsPoint(point) {
+    return this.points.find((p) => p.equals(point));
+  }
+
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
+
+  removePoint(point) {
+    const segments = this.getSegmentsWithPoint(point);
+    for (let segment of segments) {
+      this.removeSegment(segment);
+    }
+    this.points.splice(this.points.indexOf(point), 1);
+  }
+
+  getSegmentsWithPoint(point) {
+    const segments = [];
+    for (let segment of this.segments) {
+      if (segment.includes(point)) {
+        segments.push(segment);
+      }
+    }
+    return segments;
+  }
+
+  addSegment(segment) {
+    console.log('addSegment', segment);
+    this.segments.push(segment);
+  }
+
+  containsSegment(segment) {
+    return this.segments.find((s) => s.equals(segment));
+  }
+
+  tryAddSegment(segment) {
+    if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)) {
+      this.addSegment(segment);
+      return true;
+    }
+    return false;
+  }
+
+  removeSegment(segment) {
+    this.segments.splice(this.segments.indexOf(segment), 1);
+  }
+
+  dispose() {
+    this.points = [];
+    this.segments = [];
+  }
+
   draw(ctx) {
     for (let segment of this.segments) {
       segment.draw(ctx);
