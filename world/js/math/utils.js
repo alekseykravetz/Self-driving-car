17ddde07dd8bac1d3ba21e7de57c24ee20cad005
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2025-03-14 15:13:49 +0200

    World: init, Polygon Operations

--- world/js/math/utils.js
+++ world/js/math/utils.js
@@ -15,6 +15,10 @@ function getDistance(p1, p2) {
   return Math.hypot(p1.x - p2.x, p1.y - p2.y);
 }
 
+function average(p1, p2) {
+  return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+}
+
 function add(p1, p2) {
   return new Point(p1.x + p2.x, p1.y + p2.y);
 }
@@ -25,4 +29,42 @@ function subtract(p1, p2) {
 
 function scale(p, scaler) {
   return new Point(p.x * scaler, p.y * scaler);
+}
+
+function translate(location, angle, offset) {
+  return new Point(location.x + Math.cos(angle) * offset, location.y + Math.sin(angle) * offset);
+}
+
+function angle(p) {
+  return Math.atan2(p.y, p.x);
+}
+
+function getIntersection(a, b, c, d) {
+  const tTop = (d.x - c.x) * (a.y - c.y) - (d.y - c.y) * (a.x - c.x);
+  const uTop = (c.y - a.y) * (a.x - b.x) - (c.x - a.x) * (a.y - b.y);
+  const bottom = (d.y - c.y) * (b.x - a.x) - (d.x - c.x) * (b.y - a.y);
+
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(a.x, b.x, t),
+        y: lerp(a.y, b.y, t),
+        offset: t,
+      };
+    }
+  }
+
+  return null;
+}
+
+function lerp(a, b, t) {
+  return a + (b - a) * t;
+}
+
+function getRandomColor() {
+  const hue = 290 + Math.random() * 260; //not blue
+  return `hsl(${hue}, 100%, 60%)`;
 }
---
+++ world/js/primitives/polygon.js
@@ -0,0 +1,95 @@
+class Polygon {
+  constructor(points) {
+    this.points = points;
+    this.segments = [];
+    for (let i = 1; i <= points.length; i++) {
+      this.segments.push(new Segment(points[i - 1], points[i % points.length]));
+    }
+  }
+
+  static union(polygons) {
+    Polygon.multiBreak(polygons);
+    const keptSegments = [];
+    for (let i = 0; i < polygons.length; i++) {
+      for (const segment of polygons[i].segments) {
+        let keep = true;
+        for (let j = 0; j < polygons.length; j++) {
+          if (i !== j) {
+            if (polygons[j].containsSegment(segment)) {
+              keep = false;
+              break;
+            }
+          }
+        }
+        if (keep) {
+          keptSegments.push(segment);
+        }
+      }
+    }
+    return keptSegments;
+  }
+
+  static multiBreak(polygons) {
+    for (let i = 0; i < polygons.length; i++) {
+      for (let j = i + 1; j < polygons.length; j++) {
+        Polygon.break(polygons[i], polygons[j]);
+      }
+    }
+  }
+
+  static break(polygon1, polygon2) {
+    const segments1 = polygon1.segments;
+    const segments2 = polygon2.segments;
+    for (let i = 0; i < segments1.length; i++) {
+      for (let j = 0; j < segments2.length; j++) {
+        const intersection = getIntersection(segments1[i].p1, segments1[i].p2, segments2[j].p1, segments2[j].p2);
+        if (intersection && intersection.offset !== 1 && intersection.offset !== 0) {
+          const point = new Point(intersection.x, intersection.y);
+          let aux = segments1[i].p2;
+          segments1[i].p2 = point;
+          segments1.splice(i + 1, 0, new Segment(point, aux));
+          aux = segments2[j].p2;
+          segments2[j].p2 = point;
+          segments2.splice(j + 1, 0, new Segment(point, aux));
+        }
+      }
+    }
+  }
+
+  containsSegment(segment) {
+    const midpoint = average(segment.p1, segment.p2);
+    return this.containsPoint(midpoint);
+  }
+
+  containsPoint(point) {
+    const outerPoint = new Point(-1000, -1000);
+    let intersectionCount = 0;
+    for (const segment of this.segments) {
+      const intersection = getIntersection(outerPoint, point, segment.p1, segment.p2);
+      if (intersection) {
+        intersectionCount++;
+      }
+    }
+    return intersectionCount % 2 == 1;
+  }
+
+  drawSegments(ctx) {
+    for (const segment of this.segments) {
+      segment.draw(ctx, { color: getRandomColor(), width: 5 });
+    }
+  }
+
+  draw(ctx, { stroke = 'blue', lineWidth = 2, fill = 'rgba(0,0,255,0.3)' } = {}) {
+    ctx.beginPath();
+    ctx.fillStyle = fill;
+    ctx.strokeStyle = stroke;
+    ctx.lineWidth = lineWidth;
+    ctx.moveTo(this.points[0].x, this.points[0].y);
+    for (let i = 1; i < this.points.length; i++) {
+      ctx.lineTo(this.points[i].x, this.points[i].y);
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  }
+}
