b9e653e984f8d1b2fd8314c2de21afe55b1c3b03
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2025-04-05 18:24:24 +0300

    Typescript: MiniMap

--- world/js/miniMap.ts
+++ world/js/miniMap.ts
@@ -1,33 +1,45 @@
-'use strict';
 class MiniMap {
-  canvas;
-  graph;
-  size;
-  scaler;
-  ctx;
-  constructor(canvas, graph, size, scaler = 0.05) {
+  private canvas: HTMLCanvasElement;
+  private graph: Graph;
+  private size: number;
+  private scaler: number;
+  private ctx: CanvasRenderingContext2D;
+
+  constructor(
+    canvas: HTMLCanvasElement,
+    graph: Graph,
+    size: number,
+    scaler: number = 0.05,
+  ) {
     this.canvas = canvas;
     this.graph = graph;
     this.size = size;
     this.scaler = scaler;
+
     this.canvas.width = size;
     this.canvas.height = size;
-    this.ctx = canvas.getContext('2d');
+
+    this.ctx = canvas.getContext('2d')!;
   }
 
-  update(viewPoint) {
+  update(viewPoint: Point): void {
     this.ctx.clearRect(0, 0, this.size, this.size);
-    const scaledViewPoint = scale(viewPoint, -this.scaler);
+
+    const scaledViewPoint: Point = scale(viewPoint, -this.scaler);
+
     this.ctx.save();
     this.ctx.translate(
       scaledViewPoint.x + this.size / 2,
       scaledViewPoint.y + this.size / 2,
     );
     this.ctx.scale(this.scaler, this.scaler);
+
     for (const segment of this.graph.segments) {
       segment.draw(this.ctx, { width: 3 / this.scaler, color: '#BBB' });
     }
+
     this.ctx.restore();
+
     new Point(this.size / 2, this.size / 2).draw(this.ctx, {
       size: 12,
       color: 'red',
